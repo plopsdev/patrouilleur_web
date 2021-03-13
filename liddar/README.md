16969bb7323918fc04814b5b5a1d112456268e58
rob195286 <195286~ecam.be>
2021-03-13 11:32:33 +0100

    refactor gloabal et fusion des dossiers perso

--- liddar/README.md
+++ liddar/README.md
@@ -1,3 +1,6 @@
 # Fichiers :
   - ClassLiddar : contient la classe Liddar qui permet de communiquer et récupérer ses valeurs.
   - main : contient un exemple de comment utiliser la classe Liddar.
+
+# Liens :
+  - Lien pour démarrer automatiquement des commandes au démarrage : https://www.overleaf.com/read/mgwdzzpvrmyy
--- liddar/classLiddar.py
+++ liddar/classLiddar.py
@@ -8,25 +8,23 @@ class Liddar:
     Classe gérant le liddar et permettant de récupérer les valeurs 
     une fois le liddar lancé sur ros.
     
-    Commande à lancer pour démarrer ROS :
-        1. ls -l /dev | grep ttyUSB ---> 
-            si on a : crw-rw----  1 root   dialout 188,   0 Jan  3 14:59 ttyUSB
-                alors on fait : sudo chmod 666 /dev/ttyUSB0
-            si on a : crw-rw-rw-  1 root   dialout 188,   0 Jan  3 14:59 ttyUSB
-                sinon pas besoin.
-
-        2. cd ~/catkin_ws/
-        3. catkin_make
-        4. source devel/setup.bash
-        5. roslaunch rplidar_ros rplidar.launch ---> lance le liddar et permet de récupérer les info via ROS.
-        6. vérifier qu'on reçois des données avec : rostopic echo /scan
+    Lancement du liddar, lancer les commandes suivantes une fois le liddar connecté :
+        1) ls -l /dev | grep ttyUSB 
+                si on a crw-rw-----, alors il faut lancer : sudo chmod 666 /dev/ttyUSB0
+                sinon, on ne fait rien.
+        2) cd ~/catkin_ws/
+        3) catkin_make
+        4) source devel/setup.bash
+        5) roslaunch rplidar_ros rplidar.launch
+        
+        Pour vérifier qu'il fonctionne, rentrer la command suivante 
+        voir une suite de valeur : rostopic echo /scan
     """
     def __init__(self):
         self.angles = None
         self.ranges = None
 
     def __callback(self, data):
-        """Ca marche donc osef"""
         angle_increment =(data.angle_max-data.angle_min)/(len(data.ranges)-1)
         # Angle without inf
         self.angles = [data.angle_min + angle_increment*i for i in range(len(data.ranges)) if data.ranges[i] != float("Inf")]
@@ -35,7 +33,6 @@ class Liddar:
 
 
     def __listener(self):
-        """Ca marche donc osef"""
         # In ROS, nodes are uniquely named. If two nodes with the same
         # name are launched, the previous one is kicked off. The
         # anonymous=True flag means that rospy will choose a unique
@@ -49,10 +46,10 @@ class Liddar:
         Cette fonction permet d'obtenir les valeurs range et angle du liddar. 
       
         Parameters: 
-            waintingTime (int) : Le temps d'attente pour obtenir les données du capteur.
+            waintingTime (int) : Le temps d'attente max pour obtenir les données du capteur.
       
         Returns: 
-            list[list[int], list[int]] : Retourne une liste contenant comme première argument 
+            list[list[float], list[float]] : Retourne une liste contenant comme première argument 
             une liste de toutes les valeurs concernant les ranges et en second une liste des angles.      
         """
         oldTime, currentTime = time(), time()
