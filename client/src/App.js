9d858d48ccc65580440b350ea861d1e80069beea
plopsdev <jonathan.miel@outlook.com>
2021-03-14 18:35:18 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { getData, updateData } from './services';
 import {React, useState, useEffect, useCallback} from 'react';
-// import LinearProgressWithLabel from '@material-ui/core/LinearProgress';
+import LinearProgressWithLabel from '@material-ui/core/LinearProgress';
 const socket = new WebSocket ('ws://localhost:3002')
 
 
@@ -43,33 +43,88 @@ const App = () => {
         };
     }, [ openListener, messageListener ]);
     
+    const colorGenerator = (tIn) => {
+        let tMin = 20;
+        let tMax = 35;
+        let coef = 255/(tMax-tMin)
+        let tProp = tIn-tMin 
+        let colorValue = 255-tProp*coef
+        return `#FF${colorValue.toString(16)}${colorValue.toString(16)}`
+    }
+
+    function useKey(key) {
+        // Keep track of key state
+        const [pressed, setPressed] = useState(false)
+    
+        // Does an event match the key we're watching?
+        const match = event => key.toLowerCase() == event.key.toLowerCase()
+    
+        // Event handlers
+        const onDown = event => {
+            if (match(event)) setPressed(true)
+        }
+    
+        const onUp = event => {
+            if (match(event)) setPressed(false)
+        }
+    
+        // Bind and unbind events
+        useEffect(() => {
+            window.addEventListener("keydown", onDown)
+            window.addEventListener("keyup", onUp)
+            return () => {
+                window.removeEventListener("keydown", onDown)
+                window.removeEventListener("keyup", onUp)
+            }
+        }, [key])
+    
+        return pressed
+    }
+
+    const zKey = useKey('z')
+    console.log(zKey)
 
     return(
-        <div>
-            {/* <LinearProgressWithLabel variant="determinate" value={sound} />  */}
-            Sound : {sound}
-            {[0, 1, 2, 3].map((raw) => {
-                return(
-                    <div className="row" key={raw}>
-                        {thermicArray.map((thermic, index) => {
-                            if (index>=raw*4 && index<(raw*4+4)){
-                                return <div className={`index_${index}`} key={index}>{thermic}</div>
-                            }
-                        })}
-                    </div>
-                )
-            })}
-            
-            <div className = "camera">
-                <button onClick={async()=> {
-                    await updateData(!isManual)
-                    setIsManual(!isManual)
-                }}>
-                    Mode Manuel
+        <>
+            {isManual ? (
+            <div style={{display: 'flex', flexDirection:'column', marginLeft: '50px'}}>
+                <h1>Mode Manuel</h1>
+                
+                <img src="http://192.168.46.61:8081" alt="Stream" />
+                <button style={{maxWidth: '150px'}} onClick={()=> {
+                        // await updateData(!isManual)
+                        setIsManual(!isManual)
+                    }}>
+                        Mode Automatique
                 </button> 
-                {/* <img src="http://192.168.46.61:8081" alt="Stream" /> */}
-            </div>
-        </div>
+            </div>) : (
+            <div style = {{marginLeft: '50px'}}>
+                <h1>Mode Automatique</h1>
+                <LinearProgressWithLabel style={{maxWidth:'30%', marginBottom:'50px'}} variant='determinate'  value={sound} />
+                {[0, 1, 2, 3].map((raw) => {
+                    return(
+                        <div className="row" key={raw}>
+                            {thermicArray.map((thermic, index) => {
+                                if (index>=raw*4 && index<(raw*4+4)){
+                                    return <div key={index} style={{width:'30px', height: '30px', backgroundColor:colorGenerator(thermic)}}></div>
+                                }
+                            })}
+                        </div>
+                    )
+                })}
+                
+                <div className = "camera">
+                    
+                    <button style={{maxWidth: '150px', marginTop:'50px'}} onClick={()=> {
+                        // await updateData(!isManual)
+                        setIsManual(!isManual)
+                    }}>
+                        Mode Manuel
+                    </button> 
+                    
+                </div>
+            </div>)}
+        </>
     )
 }
 export default App;
